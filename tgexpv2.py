3ba6064a48b3f8c34ba1fd44675f69375b5e4335
Alexander Cock <alexander@TopazLPT-Kl.TopazDomain>
2023-10-21 15:21:14 +0100

    Mod 2 basis. Echos text sent to it.

--- tgexpv2.py
+++ tgexpv2.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 
 #Configure Logging module - use level=logging.DEBUG for more verbose info.
 logging.basicConfig(
@@ -14,6 +14,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id, 
         text="Setting out a basic platform.")
+    
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 if __name__ == '__main__':
     #Application builder. On its own, doesn't do anything.
@@ -23,7 +26,10 @@ if __name__ == '__main__':
     #The command handler. This will listen for the "\start"
     #commands.
     start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    
+    application.add_handler(start_handler) #Listens for the start command!
+    application.add_handler(echo_handler) #Listens for the second command handler!
     
     #This runs the bot until it recieves Cntrl-C from Terminal!
     application.run_polling() 
